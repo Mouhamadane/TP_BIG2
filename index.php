bf478e04f9e3593f8e689912b9834a9c19f89165
Mouhamadane <danelegrand@gmail.com>
2023-01-23 08:17:21 +0000

    ajout étudiant

--- index.php
+++ index.php
@@ -7,15 +7,61 @@ global $connect;
 
 $etudiant = new Etudiant(
    [
-      "name"=>"Mouhamadane",
-      "lastname"=>"Mboup",
+      "name"=>"Ibrahima",
+      "lastname"=>"Mbaye",
       "email"=>"danelegrand@gmail.com",
-      "datedenaissance"=>"01/02/1987"
+      "datedenaissance"=>"01/22/1977"
    ]
 );
 
 // Connexion à la base de données
 // Ajouter un etudiant dans ma base de données
+// INSERT INTO `etudiant` (`Nom`, `Prenom`, `Email`, `Birthday`) VALUES (?,?,?,?)
+
+$query = $connect->prepare('INSERT INTO `etudiant` (`Nom`, `Prenom`, `Email`, `Birthday`) VALUES (?,?,?,?)');
+$result = $query->execute([
+   $etudiant->getName(),
+   $etudiant->getLastName(),
+   $etudiant->getEmail(),
+   $etudiant->getDateDeNaissance()
+]);
+
+if($result){
+   $message = "ajouté avec succés";
+} else{
+   $message = "une erreur";
+}
+
+// Home WOrk
+// SELECT count(*) as nbr FROM `etudiant` WHERE email = '".$etudiant->getEmail()."'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 
+
+// une requête préparée 
+
+
+
+// Recupérer la liste de tous les étudiants 
+// SELECT * FROM `etudiant` ORDER BY `Nom`
+// $listQuery = $connect->query("SELECT * FROM `etudiant` WHERE 1");
+// while($data = $listQuery->fetch(PDO::FETCH_ASSOC)){
+
+
+
 
 
 
